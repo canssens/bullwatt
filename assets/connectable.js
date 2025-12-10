c558acbab35772fffaab9e5f8510fa8e5fe98880
Charles Anssens <canssens@gmail.com>
2025-12-10 22:05:32 +0100

    integrate Auuki BLE connectable

---
+++ assets/connectable.js
@@ -0,0 +1,6 @@
+// Import the project Auuki Connectable
+import Connectable from '../dependencies/auuki/src/ble/connectable.js';
+window.Connectable = Connectable;
+
+import { indoorBikeData } from '../dependencies/auuki/src/ble/ftms/indoor-bike-data.js';
+window.indoorBikeData = indoorBikeData;
